5a337e7f27b60fe614731c26325e0ba75be2ad24
Taylor Jones <taylor.jones80@hotmail.com>
2019-12-15 21:55:44 -0600

    feat: final test

--- src/Button/Button.story.tsx
+++ src/Button/Button.story.tsx
@@ -5,57 +5,35 @@ import { storiesOf } from '@storybook/react';
 const stories = storiesOf('Button', module);
 
 stories.add('Default', () => {
-  return (
-    <Buttons.DefaultButton >
-      {'Click Me'}
-    </Buttons.DefaultButton >
-  )
+  return <Buttons.DefaultButton>{'Click Me'}</Buttons.DefaultButton>;
 });
 
 stories.add('Secondary Color', () => {
-  return (
-    <Buttons.SecondayButton >
-      {'Click Me'}
-    </Buttons.SecondayButton >
-  )
+  return <Buttons.SecondayButton>{'Click Me'}</Buttons.SecondayButton>;
 });
 
 stories.add('Accent Cool Color', () => {
-  return (
-    <Buttons.AccentCoolButton >
-      {'Click Me'}
-    </Buttons.AccentCoolButton >
-  )
+  return <Buttons.AccentCoolButton>{'Click Me'}</Buttons.AccentCoolButton>;
 });
 
 stories.add('Base Color', () => {
-  return (
-    <Buttons.BaseButton >
-      {'Click Me'}
-    </Buttons.BaseButton >
-  )
+  return <Buttons.BaseButton>{'Click Me'}</Buttons.BaseButton>;
 });
 
 stories.add('Outline', () => {
-  return (
-    <Buttons.OutlineButton>
-      {'Click Me'}
-    </Buttons.OutlineButton >
-  )
+  return <Buttons.OutlineButton>{'Click Me'}</Buttons.OutlineButton>;
 });
 
 stories.add('Outline Inverse', () => {
   return (
-    <Buttons.OutlineInverseButton >
-      {'Click Me'}
-    </Buttons.OutlineInverseButton >
-  )
+    <Buttons.OutlineInverseButton>{'Click Me'}</Buttons.OutlineInverseButton>
+  );
 });
 
 stories.add('Big Button', () => {
-  return (
-    <Buttons.BigButton >
-      {'Click Me'}
-    </Buttons.BigButton >
-  )
+  return <Buttons.BigButton>{'Click Me'}</Buttons.BigButton>;
+});
+
+stories.add('TAYLOR DID IT!', () => {
+  return <div>TAYLOR DID IT</div>;
 });
